6872eb1940041405ec24393db1fc12c35317ba85
05-Afiq <91868671+05-Afiq@users.noreply.github.com>
2021-10-12 17:44:49 +0800

    Create a Script For Player Collision And Text

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public Text Coin;
+    public int CoinCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            CoinCount += 10;
+            Coin.text = "Coin: $" + CoinCount;
+
+        }
+        if (other.gameObject.tag == "Enviroment")
+        {
+            SceneManager.LoadScene("GameLOseScene");
+        }
+    }
+}
